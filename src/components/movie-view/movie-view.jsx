0e361ee6b0e8d391a7251ba3a9ddad9b009b3453
Kyle Michaels <kyle.michaels@gmail.com>
2024-05-15 13:06:15 -0700

    Created movie view component.

---
+++ src/components/movie-view/movie-view.jsx
@@ -0,0 +1,38 @@
+import { useParams } from "react-router";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import "./movie-view.scss"
+
+export const MovieView = () => {
+  const movies = useSelector((state) => state.movies.list);
+  const { movieId } = useParams();
+
+  const movie = movies.find((m) => m.id === movieId);
+
+  return (
+    <div>
+      <div>
+        <img className="w-100" src={movie.image} />
+      </div>
+      <div>
+        <span>Title: </span>
+        <span>{movie.title}</span>
+      </div>
+      <div>
+        <span>Description: </span>
+        <span>{movie.description}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movie.genreName}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movie.director}</span>
+      </div>
+      <Link to={`/`}>
+        <button className="back-button">Back</button>
+      </Link>
+    </div>
+  );
+};
