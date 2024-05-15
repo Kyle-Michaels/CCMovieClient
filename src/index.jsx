f008c73c0a1d85c36c7c067b0685ecd88b0d6454
Kyle Michaels <kyle.michaels@gmail.com>
2024-05-15 12:03:58 -0700

    Created component files.

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,19 @@
 import { createRoot } from 'react-dom/client';
+import { MainView } from './components/main-view/main-view';
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+import Container from 'react-bootstrap/Container';
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 
 const MyFlixApplication = () => {
   return (
-    <div className='my-flix'>
-      <div>Good morning</div>
-    </div>
+    <Provider store={store}>
+      <Container>
+        <MainView />
+      </Container>
+    </Provider>
   );
 };
 
